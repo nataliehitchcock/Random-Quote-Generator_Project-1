b7e1ed656a120447d3aca0efba96e0a301e142eb
nataliehitchcock <56402315+nataliehitchcock@users.noreply.github.com>
2019-10-21 21:33:05 -0600

    Added a tags property and updated my p tags to span elements for citation, year and tags.

--- js/script.js
+++ js/script.js
@@ -10,31 +10,36 @@ var quotes = [{
   quote: 'Without music, life would be a mistake',
   source: 'Friedrich Nietzsche',
   citation: 'Brainpickings: Nietzsche on the Power of Music: https://www.brainpickings.org/2015/09/18/nietzsche-on-music/',
+  tags: '#philospher',
 },
 
 {
   quote: 'Music should be an essential part of every analysis.',
   source: 'Carl Jung',
   citation: 'https://www.udiscovermusic.com/stories/15-inspirational-quotes-power-music/',
-  year: '1875-1961'
+  year: '1875-1961',
+  tags: '#scientist',
 },
 
 {
   quote: 'Where words fail, music speaks.',
   source: 'Hans Christian Andersen',
   citation: 'https://www.cmuse.org/100-famous-and-inspirational-music-quotes/',
+  tags: '#composer',
 },
 
 {
   quote: 'Music touches us emotionally, where words alone can’t',
   source: 'Johnny Depp',
   citation: 'https://www.cmuse.org/100-famous-and-inspirational-music-quotes/',
+  tags: '#actor',
 },
 
 {
   quote: 'Music is by far the most wonderful method we have to remind us each day of the power of personal accomplishment.',
   source: 'Chris S. Salazar,',
   citation: 'https://www.wow4u.com/musicquotes/',
+  tags: '#inspirational',
 }
 ];
 // Created a random quote number generator that pulls from the quotes array
@@ -48,16 +53,19 @@ function printQuote() {
 var message = "";    
 var result = getRandomQuote(quotes);
 message = "<p class='quote'>" + result.quote + "</p>";
-message += "<p class='source'>" + result.source;
+message += "<p class='source'>" + result.source; 
 
 if (result.citation) {
-message += "<p class='citation'>" + result.citation;
+message += <span class='citation'> ${result.citation}</span>
 }
-
 if (result.year) {
-message += "<p class= 'year'>" + result.year;
-message += "</p>";
+message += <span class='year'> ${result.year}</span>
 }
+if (result.tags){
+message += <span class='tags'> ${result.tags}</span>
+}
+message += "</span>" + "</p>";
+
 
 document.getElementById('quote-box').innerHTML = message;
 
